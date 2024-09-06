360fb59ea1aae7bdea8e6129f2de08f5c2aba90d
VineetDarji <142860186+VineetDarji@users.noreply.github.com>
2024-09-06 11:04:08 +0530

    Add files via upload

---
+++ project/finance_advisor.php
@@ -0,0 +1,66 @@
+<?php  
+
+include 'components/connect.php';
+
+if(isset($_COOKIE['user_id'])){
+   $user_id = $_COOKIE['user_id'];
+}else{
+   $user_id = '';
+}
+
+if(isset($_GET['get_id'])){
+   $get_id = $_GET['get_id'];
+}else{
+   $get_id = '';
+   header('location:home.php');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php include 'components/user_header.php'; ?>
+<H1>hELLO</H1>
+
+</body>
+</html>
+
+
+
+<script src="js/script.js"></script>
